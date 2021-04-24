165631547ae00ce094481adda97ba9c3092edb82
Marcus <46413447+maradder@users.noreply.github.com>
2021-04-24 03:21:02 -0700

    I had some extra awakeness in me.  Feel free to cut /paste/delete

---
+++ hackathon/src/js/temp.js
@@ -0,0 +1,5 @@
+let qrDiv = document.querySelector('.qrCode');
+
+let tempQrCode = document.createElement("img");
+let image = tempQrCode.setAttribute("src", "/src/img/websiteQRCode_noFrame 1.png")
+qrDiv.appendChild(tempQrCode);
